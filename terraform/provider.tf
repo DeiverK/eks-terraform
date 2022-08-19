616f70d84a1663474dcbfcf2b64557d7c927ca6b
Deiver Kiel <deikiel@gmail.com>
2022-08-19 10:51:18 -0600

    provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,5 +19,6 @@ provider "aws" {
 provider "kubernetes" {
     host = data.aws_eks_cluster.cluster.endpoint
     token = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.[0].data)
+    #cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
