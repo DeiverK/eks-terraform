11ad57a010d11f0aee169ee0534dac39ed6691e4
Deiver Kiel <deikiel@gmail.com>
2022-08-19 10:48:49 -0600

    provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,14 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
-  }
-}
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "aws"
+#     # This requires the awscli to be installed locally where Terraform is executed
+#     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+#   }
+# }
 
 provider "aws" {
     region = "us-east-1"
@@ -16,8 +16,8 @@ provider "aws" {
 }
 
 
-# provider "kubernetes" {
-#     host = data.aws_eks_cluster.cluster.endpoint
-#     token = data.aws_eks_cluster_auth.cluster.token
-#     cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-# }
+provider "kubernetes" {
+    host = data.aws_eks_cluster.cluster.endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
